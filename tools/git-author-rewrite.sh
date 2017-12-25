1c2371998ed8238b5285ed6558642b96ffa0f178
songjinzhong <648660165@qq.com>
2017-12-25 14:41:19 +0800

    update add tools

---
+++ tools/git-author-rewrite.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="yuren.sjz@alibaba-inc.com"
+CORRECT_NAME="songjinzhong"
+CORRECT_EMAIL="648660165@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
